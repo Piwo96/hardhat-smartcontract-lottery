cf899edac09df57fc00fd95814b3d87276b1b52d
Piwo96 <philipp.rollmann@online.de>
2022-08-11 10:22:35 +0200

    Created lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,2 +1,180 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+error Lottery__LotteryNotOpen();
+error Lottery__NotEnoughEthEntered();
+error Lottery_UpkeepNotNeeded(
+    uint256 currentBalance,
+    uint256 numPlayers,
+    uint256 lotteryState
+);
+error Lottery__TransferFailed();
+
+/**
+ * @title A sample lottery contract
+ * @author Philipp rollmann
+ * @notice This contract is for creating an unteamperable decentralized smart contract
+ * @dev This implements Chainlink VRF v2 and Chanlink Keepers
+ */
+contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    /* Types */
+    enum LotteryState {
+        OPEN,
+        CALCULATING
+    }
+
+    /* State variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    uint256[] public s_randomWords;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscribtionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Lottery variables
+    address private s_recentWinner;
+    LotteryState private s_lotteryState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
+
+    /* Everts */
+    event LotteryEnter(address indexed player);
+    event RequestLotteryWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscribtionId,
+        uint32 callbackGasLimit,
+        uint256 interval
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscribtionId = subscribtionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lotteryState = LotteryState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
+    }
+
+    function enterLottery() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughEthEntered();
+        }
+        if (s_lotteryState != LotteryState.OPEN) {
+            revert Lottery__LotteryNotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        emit LotteryEnter(msg.sender);
+    }
+
+    /**
+     * @dev This is the function that the Chainlink Keeper node call
+     * they look for the `upkeepNeeded`to return true
+     * The fallowing should be true in order to return true;
+     * 1. Our time interval should have passed
+     * 2. The lottery should have at least 1 player, and have some ETH
+     * 3. Our subscription is funded with LINK
+     * 4. The lottery should be in an "open" state
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool isOpen = LotteryState.OPEN == s_lotteryState;
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = (address(this).balance != 0);
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+        return (upkeepNeeded, "0x0");
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Lottery_UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_lotteryState)
+            );
+        }
+        s_lotteryState = LotteryState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscribtionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestLotteryWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256,
+        /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_lotteryState = LotteryState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Lottery__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    /* View / Pure functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getRecentwinner() public view returns (address) {
+        return s_recentWinner;
+    }
+
+    function getLotteryState() public view returns (LotteryState) {
+        return s_lotteryState;
+    }
+
+    function getNumberOfWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLatestTimestamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
+    }
+}
