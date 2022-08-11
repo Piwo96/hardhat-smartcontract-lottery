8e215e6fd1e51213bf65d20f0a5c0335bb2f6e3d
Piwo96 <philipp.rollmann@online.de>
2022-08-11 23:05:03 +0200

    Deployment script for lottery contract created

---
+++ deploy/01-deploy-lottery.ts
@@ -0,0 +1,64 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { networkConfig, developmentChains } from "../helper-hardhat-config";
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30");
+
+const deployLottery: DeployFunction = async function (
+    hre: HardhatRuntimeEnvironment
+) {
+    const { getNamedAccounts, deployments, network } = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId: number = network.config.chainId!;
+    const chainName: string = network.name;
+
+    let vrfCoordinatorV2Address: string, subscriptionId: string;
+    const waitConfirmations: number =
+        networkConfig[chainId].blockConfirmations || 1;
+    if (developmentChains.includes(chainName)) {
+        const vrfCoordinatorV2 = await ethers.getContract(
+            "MockVrfCoordinatorV2"
+        );
+        vrfCoordinatorV2Address = vrfCoordinatorV2.address;
+        const transactionResponse = await vrfCoordinatorV2.createSubscription();
+        const transactionReceipt = await transactionResponse.wait(1);
+        subscriptionId = transactionReceipt.events[0].args.subId;
+        // Fund subscription
+        // Usually, you'd need the link token on a real network
+        await vrfCoordinatorV2.fundSubscription(
+            subscriptionId,
+            VRF_SUB_FUND_AMOUNT
+        );
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2!;
+        subscriptionId = networkConfig[chainId].subscriptionId!;
+    }
+
+    const entranceFee = networkConfig[chainId].entranceFee;
+    const gasLane = networkConfig[chainId].gasLane;
+    const callbackGasLimit = networkConfig[chainId].callbackGasLimit!;
+    const interval = networkConfig[chainId].interval;
+
+    const args: any = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        callbackGasLimit,
+        interval,
+    ];
+    await deploy("Lottery", {
+        contract: "Lottery",
+        from: deployer,
+        log: true,
+        args: args,
+        waitConfirmations: waitConfirmations,
+    });
+
+    log("Lottery deployed!");
+    log("---------------------------------------------");
+};
+
+export default deployLottery;
+deployLottery.tags = ["all", "lottery"];
